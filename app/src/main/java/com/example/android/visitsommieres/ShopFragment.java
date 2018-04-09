17b724652d184b66540f5d7b61b6bf4b1ae52863
fruitcakej <33979363+fruitcakej@users.noreply.github.com>
2018-04-09 11:17:41 +0200

    fixes

--- app/src/main/java/com/example/android/visitsommieres/ShopFragment.java
+++ app/src/main/java/com/example/android/visitsommieres/ShopFragment.java
@@ -3,7 +3,7 @@ package com.example.android.visitsommieres;
 import android.support.v4.app.Fragment;
 
 /**
- * Created by Chloe10 on 29/03/2018.
+ * Created by JamieC on 29/03/2018.
  */
 
 public class ShopFragment extends Fragment{
