6aa607ef6b9c9f988b898e3221ef66988418779f
fruitcakej <33979363+fruitcakej@users.noreply.github.com>
2018-04-09 16:18:59 +0200

    updates

--- app/src/main/java/com/example/android/visitsommieres/ShopFragment.java
+++ app/src/main/java/com/example/android/visitsommieres/ShopFragment.java
@@ -1,10 +1,56 @@
 package com.example.android.visitsommieres;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by JamieC on 29/03/2018.
+ * Created by JamieC on 09/04/2018.
  */
 
 public class ShopFragment extends Fragment{
+
+    List<Items> itemsList;
+    RecyclerView recyclerView;
+
+    public ShopFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        recyclerView = (RecyclerView) inflater.inflate(R.layout.layout_recycler, container, false);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recyclerView.setBackgroundResource(0);
+
+        itemsList = new ArrayList<>();
+
+
+
+
+
+
+
+
+
+        ItemsAdapter adapter = new ItemsAdapter(getActivity(), itemsList);
+
+        recyclerView = recyclerView.findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(adapter);
+
+        return recyclerView;
+    }
 }
